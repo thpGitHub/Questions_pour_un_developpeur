47bb5a3fdb88d71290d306f235631d203e213c87
thierry <thierrypoupon@hotmail.com>
2020-11-16 20:49:42 +0100

    clean code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,28 +1,16 @@
-const check = {
-            fields: '',
-            pseudo_password: ''
-};
 
 document.querySelector('input[type="submit"]').addEventListener('click', (e) => {
     e.preventDefault();
 
-    // check empty fields
     if (document.querySelector('input[type="text"]')    .value === '' ||
         document.querySelector('input[type="password"]').value === '') {
-        //e.preventDefault();
+
         document.querySelector('#message')              .innerHTML = 'veuillez remplir tous les champs';
-        /*document.querySelector('input[type="text"]')    .value = '';
-        document.querySelector('input[type="password"]').value = '';*/
         document.querySelector('form').reset();
-
-        check.fields = 'ko';
-
     }else {
-        check.fields = 'ok';
-
         // AJAX request for check pseudo and email
         const request = new XMLHttpRequest(),
-            url     = '/verify_login';// route pour le serveur express
+              url     = '/verify_login';// route pour le serveur express
 
         request.open('post', url);
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -32,26 +20,13 @@ document.querySelector('input[type="submit"]').addEventListener('click', (e) => 
             console.log('la reponse = ', request.response);
             if (request.response === 'ko') {
                 document.querySelector('#message')              .innerHTML = 'pseudo ou mot de passe incorrect';
-                /*document.querySelector('input[type="text"]')    .value = '';
-                document.querySelector('input[type="password"]').value = '';*/
                 document.querySelector('form').reset();
-                check.pseudo_password = 'ko';
             }
             else  {
-                check.pseudo_password = 'ok';
-                console.log('check ok dans login.js = ', check);
                 document.querySelector('form').submit();
             }
         };
     }
-    /*if (check.fields === 'ok' && check.pseudo_password === 'ok') {
-        //window.location = '/save_registration_in_db';
-        console.log('check ok dans login.js = ', check);
-        //window.location = "/game";
-    }
-    else {
-        e.preventDefault();
-    }*/
 });
 
 //vérification au keyup si tous les champs sont remplis on supprime le message 'veuillez remplir tous les champs'
